3648c2470962268f4ba9ff473348f4d45a773420
EdJeeOnGitHub <edjee96@gmail.com>
2021-02-17 19:51:02 -0600

    initial commit for packages

---
+++ install.R
@@ -0,0 +1,17 @@
+package_vector <- c(
+	"rstan",
+	"bayesplot",
+	"haven",
+	"ggstance",
+	"tidybayes",
+	"shinystan",
+	"fixest",
+	"lfe",
+	"sandwich",
+	"AER",
+	"vroom",
+	"rstanarm",
+	"MASS",
+	"brms")
+install.packages(package_vector)
+devtools::install_github("rmcelreath/rethinking@2.13")
